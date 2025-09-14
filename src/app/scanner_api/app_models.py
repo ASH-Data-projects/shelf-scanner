c807b0970b88a0038f1b321fac363880e520e90a
Shadek <shadekjaramillo@gmail.com>
2025-09-13 20:05:07 -0500

    now the order model returns a tuple containing two DataFrames, the first one tells if the scanned shelf complies with the order, the second one has the parsed results of the yolo model.

--- src/app/scanner_api/app_models.py
+++ src/app/scanner_api/app_models.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from ultralytics.engine.results import Results
@@ -8,14 +7,16 @@ from typing import Any
 from .scanner_classes import ScannerResult
 
 class OrderModel:
-    def __init__(self, shelf_csv=None):
-        if shelf_csv:
-            df = pd.read_csv(
-                shelf_csv,
-                sep=' ',
-                names=['cls','x','y','w','h']
-            )
-            self.select_order(df)
+    def __init__(self, shelf_csv):
+        df = pd.read_csv(
+            shelf_csv,
+            sep=' ',
+            names=['cls','x','y','w','h']
+        )
+        df.sort_values('y',inplace=True, ignore_index=True)
+        df['pos'] = range(df.shape[0])
+        self.base_shelf = df[['x', 'y', 'w', 'h', 'cls', 'pos']]
+        self.select_order(df)
     
     @staticmethod
     def _preprocess_boxes_coordinates(df: pd.DataFrame):
@@ -28,20 +29,28 @@ class OrderModel:
     def select_order(self, df:pd.DataFrame):
         df = self._preprocess_boxes_coordinates(df)
         self.position_finder = KNeighborsClassifier(n_neighbors=1)
-        self.position_finder.fit(df[['x','y']], np.arange(df.shape[0]))
+        self.position_finder.fit(df[['x','y']], df.pos)
     
-    def predict(self, input:pd.DataFrame|Results) -> pd.DataFrame:
-        if isinstance(input, Results):        
-            x,y,h,w = input.boxes.xywh.T
-            cls = input.boxes.cls
-            data = {'x': x, 'y': y, 'w': w, 'h': h, 'cls': cls}
-            input = pd.DataFrame(data)
+    def predict(self, input:Results) -> pd.DataFrame:
+        x,y,h,w = input.boxes.xywh.T
+        cls = input.boxes.cls
+        data = {'x': x, 'y': y, 'w': w, 'h': h, 'cls': cls}
+        detection = pd.DataFrame(data)
             
-        df = self._preprocess_boxes_coordinates(input)
-        position = self.position_finder.predict(df[['x','y']])
+        detection = self._preprocess_boxes_coordinates(detection)
+        detection['pos'] = self.position_finder.predict(detection[['x','y']])
+        detection['SKU'] = detection['cls'].map(input.names)
+        detection.sort_values('pos', ignore_index=True, inplace=True)
         
-        return position
-    
+        def check_item(row):
+            row = row[['cls','pos']]
+            return (detection[['cls','pos']] == row).all(axis=1).any()
+        
+        comparison_df = self.base_shelf[['pos', 'cls']].copy()
+        comparison_df['SKU'] = comparison_df['cls'].map(input.names)
+        comparison_df['matches'] = comparison_df.apply(check_item, axis=1)
+        
+        return (comparison_df, detection)
 
 class Scanner:
     """
