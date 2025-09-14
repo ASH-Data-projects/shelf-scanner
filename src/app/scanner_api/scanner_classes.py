cd93d251143fe55c163654bf3a61aba0258eac35
Shadek <shadekjaramillo@gmail.com>
2025-09-14 01:27:56 -0500

    wrote de docstring for the ScannerResult object.

--- src/app/scanner_api/scanner_classes.py
+++ src/app/scanner_api/scanner_classes.py
@@ -5,10 +5,16 @@ from dataclasses import dataclass, field
 @dataclass
 class ScannerResult:
     """
-    dataclass containing the information that is returned by the scanner
+    dataclass to store the data computed by the Scanner model.
+    Args:
+        yolo_result (Results): Result of the YOLO model
+        
+        order_result (tuple[pd.DataFrame, pd.DataFrame]): Result of the 
+        OrderModel model.
+
     """
     yolo_result: Results = field(repr=False)
-    order_result: pd.DataFrame
+    order_result: tuple[pd.DataFrame, pd.DataFrame]
     
     def __post_init__(self):
         pass
